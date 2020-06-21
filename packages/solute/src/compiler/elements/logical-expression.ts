cae5d97305d2ed0d9b71c8966a97d48994956678
tanmoyopenroot <tanmoy.openroot@gmail.com>
2020-06-21 08:34:42 +0530

    feat(element): create logical expression element

---
+++ packages/solute/src/compiler/elements/logical-expression.ts
@@ -0,0 +1,67 @@
+import { x, b } from 'code-red';
+import { BaseNode, Expression, LogicalExpression } from 'estree-jsx';
+
+import Block from '../block';
+import BaseElement from './base-element';
+import createVariable from '../utils/create-variable';
+
+export default class LogicalExpressionElement extends BaseElement {
+  private node: LogicalExpression;
+  private blockFunction: string;
+  private variable: string;
+  constructor(node: LogicalExpression) {
+    super();
+
+    this.type = 'LogicalExpressionElement';
+    this.node = node;
+
+    this.generateVariable();
+    this.attachVariable();
+  }
+
+  private generateVariable() {
+    this.variable = createVariable('ifBlock');
+    this.blockFunction = createVariable('createIfBlock');
+  }
+
+  private attachVariable() {
+    Reflect.defineProperty(
+      this.node,
+      'variable',
+      { value: this.variable },
+    );
+  }
+
+  public generateConditionalBlock(): BaseNode[] {
+    const { right } = this.node;
+    return new Block(right).generate(this.blockFunction);
+  }
+
+  public generateDelcaration(): BaseNode {
+    const decalaration =  b`let ${this.variable}`;
+    return decalaration[0];
+  }
+
+  public generateCreate(): BaseNode[] {
+    const {
+      left,
+      operator,
+    } = this.node;
+
+    // Need to fix this
+    // 1: Operator
+    // 2: Return type
+
+    return [
+      x`${this.variable} = ${left} && ${this.blockFunction}()`,
+      x`${this.variable} && ${this.variable}.create.call(this)`
+    ];
+  }
+
+  public generateMount(parent?: BaseNode): Expression {
+    const target = (parent && parent['variable']) || 'target';
+    return x`${this.variable} && ${this.variable}.mount.call(this, ${target})`;
+  }
+}
+
+export { LogicalExpressionElement };
