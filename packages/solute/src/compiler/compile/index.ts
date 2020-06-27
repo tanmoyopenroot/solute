e4cefbede29beeb4702cd99fd1af6554f78be496
tanmoyopenroot <tanmoy.openroot@gmail.com>
2020-06-27 07:19:26 +0530

    fix(compile): add component generator to complie

--- packages/solute/src/compiler/compile/index.ts
+++ packages/solute/src/compiler/compile/index.ts
@@ -1,19 +1,14 @@
 import { BaseNode } from 'estree-jsx';
 
-// import Component from '../component';
+import Component from '../component';
 import Parser, { TOption } from '../parser';
 
 const defaultOptions = {
   allowImportExportEverywhere: true,
 };
-export default (
-  source: string,
-  options: TOption = defaultOptions,
-): BaseNode => {
-  // const ast = Parser.parse(source, options);
-  // const component = new Component(ast);
+export default (source: string, options: TOption = defaultOptions): BaseNode[] => {
+  const ast = Parser.parse(source, options);
+  const component = new Component(ast);
 
-  // component.generate();
-
-  return Parser.parse(source, options);
+  return component.generate();
 };
