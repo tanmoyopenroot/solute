cda25d3b5e2c6c086f82f0c2d0f6f371381c77bd
tanmoyopenroot <tanmoy.openroot@gmail.com>
2020-07-05 07:15:49 +0530

    feat(property): create property extractor class

---
+++ packages/solute/src/compiler/component/properties/extractor.ts
@@ -0,0 +1,184 @@
+import { walk } from 'estree-walker';
+import { BaseNode, ExportDefaultDeclaration, Expression, ObjectExpression, Statement } from 'estree-jsx';
+
+import DataProperty from './data';
+import ComputedProperty from './computed';
+import WatchProperty from './watch';
+import MethodsProperty from './methods';
+
+const COMPONENT_PROPERTY = {
+  NAME: 'name',
+  PROPS: 'props',
+  DATA: 'data',
+  COMPUTED: 'computed',
+  WATCH: 'watch',
+  METHODS: 'methods',
+  RENDER: 'render',
+};
+
+export default class Extractor {
+  private nodes: {
+    props: ObjectExpression;
+    data: ObjectExpression;
+    computed: ObjectExpression;
+    watch: ObjectExpression;
+    methods: ObjectExpression;
+    render: Expression;
+  };
+
+  private name: string;
+  constructor(ast: BaseNode) {
+    this.nodes = {
+      props: null,
+      data: null,
+      computed: null,
+      watch: null,
+      methods: null,
+      render: null,
+    };
+
+    this.extractProperties(ast);
+  }
+
+  private setNode<T>(type: string, node: T) {
+    this.nodes[type] = node;
+  }
+
+  private setName(name: string) {
+    this.name = name;
+  }
+
+  private isRenderBody(body: Statement[]): boolean {
+    if (body.length !== 1) {
+      return false;
+    }
+
+    return body[0].type === 'ReturnStatement';
+  }
+
+  private extractProperties(ast: BaseNode) {
+    const self = this;
+
+    walk(ast, {
+      enter(node) {
+        if (node.type === 'ExportDefaultDeclaration') {
+          const { declaration } = node as ExportDefaultDeclaration;
+
+          if (declaration.type === 'ObjectExpression') {
+            declaration.properties.forEach((property) => {
+              if (property.type === 'Property' && property.key.type === 'Identifier') {
+                switch (property.key.name) {
+                  case COMPONENT_PROPERTY.NAME:
+                    if (property.value.type === 'Literal' && typeof property.value.value === 'string') {
+                      self.setName(property.value.value);
+                    }
+                    break;
+
+                  case COMPONENT_PROPERTY.PROPS:
+                    if (property.value.type === 'ObjectExpression') {
+                      self.setNode(COMPONENT_PROPERTY.PROPS, property.value);
+                    }
+
+                    break;
+
+                  case COMPONENT_PROPERTY.DATA:
+                    if (property.value.type === 'ObjectExpression') {
+                      self.setNode(COMPONENT_PROPERTY.DATA, property.value);
+                    }
+
+                    break;
+
+                  case COMPONENT_PROPERTY.COMPUTED:
+                    if (property.value.type === 'ObjectExpression') {
+                      self.setNode(COMPONENT_PROPERTY.COMPUTED, property.value);
+                    }
+
+                    break;
+
+                  case COMPONENT_PROPERTY.WATCH:
+                    if (property.value.type === 'ObjectExpression') {
+                      self.setNode(COMPONENT_PROPERTY.WATCH, property.value);
+                    }
+
+                    break;
+
+                  case COMPONENT_PROPERTY.METHODS:
+                    if (property.value.type === 'ObjectExpression') {
+                      self.setNode(COMPONENT_PROPERTY.METHODS, property.value);
+                    }
+
+                    break;
+
+                  case COMPONENT_PROPERTY.RENDER:
+                    if (property.value.type === 'FunctionExpression') {
+                      const { body } = property.value;
+
+                      if (body.body.length !== 1) {
+                        return;
+                      }
+
+                      const returnNode = body.body[0];
+
+                      if (returnNode.type === 'ReturnStatement') {
+                        self.setNode(COMPONENT_PROPERTY.RENDER, returnNode.argument);
+                      }
+                    }
+
+                    break;
+                }
+              }
+            });
+          }
+
+          this.skip();
+        }
+      },
+    });
+  }
+
+  public generateData(): { node: BaseNode[]; dependencies: Record<string, boolean> } {
+    const data = new DataProperty(this.nodes[COMPONENT_PROPERTY.DATA]);
+
+    const node = data.generateNode();
+    const dependencies = data.getDependencies();
+
+    return {
+      node,
+      dependencies,
+    };
+  }
+
+  public generateComputed(): { node: BaseNode[]; dependencies: Record<string, boolean> } {
+    const data = new ComputedProperty(this.nodes[COMPONENT_PROPERTY.COMPUTED]);
+
+    const node = data.generateNode();
+    const dependencies = data.getDependencies();
+
+    return {
+      node,
+      dependencies,
+    };
+  }
+
+  public generateMethods(dependencies: Record<string, boolean>): { node: BaseNode[] } {
+    const data = new MethodsProperty(this.nodes[COMPONENT_PROPERTY.METHODS], dependencies);
+    const node = data.generateNode();
+
+    return { node };
+  }
+
+  public generateWatch(): { node: BaseNode[] } {
+    const data = new WatchProperty(this.nodes[COMPONENT_PROPERTY.WATCH]);
+    const node = data.generateNode();
+
+    return { node };
+  }
+
+  public getRenderNode(): BaseNode {
+    return this.nodes[COMPONENT_PROPERTY.RENDER];
+  }
+
+  public getComponentName(): string {
+    return this.name;
+  }
+}
