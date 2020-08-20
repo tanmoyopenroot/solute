ccecb4ad56441ef9b8c4b9bcfaebe4e6b69e07e4
tanmoyopenroot <tanmoy.openroot@gmail.com>
2020-08-20 20:12:28 +0530

    test(elements): add test

--- packages/solute/test/compile/index.spec.js
+++ packages/solute/test/compile/index.spec.js
@@ -1,25 +1,47 @@
 const fs = require('fs');
 const path = require('path');
-const { compile } = require('../../dist');
+const { print } = require('code-red');
+const { compile } = require('../../dist/index');
 
-describe('compile', () => {
-	fs.readdirSync(`${__dirname}/samples`).forEach(dir => {
-    const resolved = path.resolve(`${__dirname}/samples`, dir);
+const featuresToTest = [
+  {
+    description: 'Compile: JSX',
+    pathToTestFiles: `${__dirname}/jsx`,
+  },
+  {
+    description: 'Compile: Reactivity',
+    pathToTestFiles: `${__dirname}/reactivity`,
+  },
+  {
+    description: 'Compile: Conditional',
+    pathToTestFiles: `${__dirname}/conditional`,
+  },
+];
 
-		if (!fs.existsSync(`${resolved}/input.jsx`)) {
-			return;
-		}
+const runTest = ({ description, pathToTestFiles }) => {
+  describe(description, () => {
+    fs.readdirSync(pathToTestFiles).forEach((dir) => {
+      const resolved = path.resolve(pathToTestFiles, dir);
 
-		test(dir, () => {
-			try {
-        const input = fs.readFileSync(`${resolved}/input.jsx`, 'utf-8').replace(/\s+$/, '');
-        const actual = compile(input);
-        const output = `${resolved}/actual.json`;
-        fs.writeFileSync(output, JSON.stringify(actual));
-			} catch (err) {
-				console.log(err.frame);
-				throw err;
-			}
-		});
-	});
-});
+      if (!fs.existsSync(`${resolved}/input.jsx`)) {
+        return;
+      }
+
+      test(dir, () => {
+        try {
+          const input = fs.readFileSync(`${resolved}/input.jsx`, 'utf-8').replace(/\s+$/, '');
+          const actual = compile(input);
+          const output = `${resolved}/actual.json`;
+          const JSoutput = `${resolved}/actual.js`;
+          fs.writeFileSync(output, JSON.stringify(actual));
+          fs.writeFileSync(JSoutput, print(actual).code);
+        } catch (err) {
+          console.log(err.frame);
+          throw err;
+        }
+      });
+    });
+  });
+};
+
+featuresToTest.forEach(runTest);
